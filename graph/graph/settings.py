d4d20d17c0962dfefd53a79afd9025d3ab00af15
Ivan <65046208+Ivan69-tech@users.noreply.github.com>
2023-12-07 22:34:00 +0100

    ok

--- graph/graph/settings.py
+++ graph/graph/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "localhost", "ivandecharbo.pythonanywhere.com"]
 
 
 # Application definition
